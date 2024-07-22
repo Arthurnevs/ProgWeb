2ddb75c5e71648ae167db4d465f9d81e0527f631
arthurneves <dev6@beeteller.com>
2024-07-22 08:43:56 -0300

    feat: lab 2

---
+++ src/server.ts
@@ -0,0 +1,74 @@
+import express, { Request, Response } from 'express';
+
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+
+let users: { id: number, name: string, document: string }[] = [
+  { id: 1, name: 'Arthur', document: "12147635456" },
+  { id: 2, name: 'Andre', document: "27262260430" },
+  { id: 3, name: 'Carlos', document: "93244564432" },
+];
+
+app.get('/users', (req: Request, res: Response) => {
+  res.json(users);
+});
+
+app.get('/users/:id', (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const user = users.find(user => user.id === id);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send('User not found');
+  }
+});
+
+
+app.post('/register', (req: Request, res: Response) => {
+  const new_user = {
+    id: users.length + 1,
+    name: req.body.name,
+    document: req.body.document
+  };
+  users.push(new_user);
+  res.status(201).json(new_user);
+});
+
+
+app.put('/users/:id', (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const userIndex = users.findIndex(user => user.id === id);
+  if (userIndex !== -1) {
+    users[userIndex] = {
+      id: id,
+      name: req.body.nome,
+      document: users[userIndex].document
+    };
+    res.json(users[userIndex]);
+  } else {
+    res.status(404).send('User not found');
+  }
+});
+
+
+app.delete('/users/:id', (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  users = users.filter(user => user.id !== id);
+  res.status(204).send();
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
+
+// https://docs.google.com/document/d/19z913LVtDRj6j5_ehT3MiPNx3TUikRAtIKuZfYlE_qo/edit
